d4e55520fb3b96a467f800d2134d2f7da005b37f
aymitator_cli <hollawac@outlook.com>
2025-11-03 08:16:02 -0800

    much_better_swing

--- org/coreyh/wordbcheat/CheatWindow.java
+++ org/coreyh/wordbcheat/CheatWindow.java
@@ -5,17 +5,20 @@ import java.awt.*;
 import java.awt.event.*;
 public class CheatWindow extends JPanel implements ActionListener {
 public JFrame win;
-public JButton btn;
+public JButton btn,btnii;
 public JLabel lbl1,lblii,lbliii;
 public JTextField t1,tii,tiii;
+
 public CheatWindow() {
-win = new JFrame("Wordb Cheat Window");
+win = new JFrame("word adder");
 win.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 GridLayout gl =
-  new GridLayout(7,1);
+  new GridLayout(4,2);
 setLayout(gl);
 btn = new JButton("Submit");
 btn.addActionListener(this);
+btnii = new JButton("Submitii");
+btnii.addActionListener(this);
 t1 = new JTextField();
 tii = new JTextField();
 tiii = new JTextField();
@@ -26,15 +29,22 @@ add(tii);
 add(new JLabel("ideaii:"));
 add(tiii);
 add(btn);
-setSize(new Dimension(500,500));
+add(btnii);
 win.setContentPane(this);
 win.pack();
+win.setSize(new
+  Dimension(
+    680, 380));
 win.setVisible(true);
 }
 public static void out(String _s) {
 System.out.println(_s);
 }
 public void actionPerformed(ActionEvent _e) {
+if(((JButton)_e.getSource()).equals(btnii)) {
+out("Writing to todo");
+}
+else {
 out("perl data-add " +
 t1.getText() + 
 " " +
@@ -42,7 +52,17 @@ tii.getText() +
 " " +
 tiii.getText());
 }
+}
 public static void main(String[] args) {
+try{
+  for(UIManager.LookAndFeelInfo i : 
+    UIManager.getInstalledLookAndFeels()) {
+if("Nimbus".equals(i.getName())){
+UIManager.setLookAndFeel(i.getClassName());break;
+}
+  }
+}catch (Exception _ez){}
+
 CheatWindow trans = new CheatWindow();
 }
 }
